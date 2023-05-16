dfb711c9d2afc510405f9b40471cce2e79488430
Marco Rabelink <marco.rabelink@gmail.com>
2023-05-16 12:27:51 +0200

    first checkin

---
+++ summarize_pdf/main.py
@@ -0,0 +1,25 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import TokenTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.summarize import load_summarize_chain
+
+from dotenv import load_dotenv
+
+# Load environment variables with path to .env file
+load_dotenv(dotenv_path='../.env')
+
+
+
+loader = PyPDFLoader('./assets/Little-book-of-Deep-Learning.pdf')
+data = loader.load()
+
+text_splitter = TokenTextSplitter(model_name='en_core_web_sm', chunk_size=4000, chunk_overlap=200)
+docs = text_splitter.split_documents(data)
+
+llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo')
+
+sum_chain = load_summarize_chain(llm, chain_type="refine", verbose=True)
+
+summary = sum_chain.run(docs)
+
+
